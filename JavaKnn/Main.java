33175188f917bcd3251b2f72ac0693f6c630fa2b
AbHXn <asofficial47@gmail.com>
2025-10-25 18:37:01 +0530

    Update Main.java

--- JavaKnn/Main.java
+++ JavaKnn/Main.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class Main{
 	public static void main(String[] args){
-        KNN test = new KNN("./train_data", "./library", "./runner", 31);
+        KNN test = new KNN("./train_data", "./library", "./runner", 7);
         File dir = new File("./runner");
         // load training data
         boolean isLoaded = TrainData.load_training_data("./train_data");
