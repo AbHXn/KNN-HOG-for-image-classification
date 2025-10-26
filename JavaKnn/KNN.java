17989e1a2bf0f35ed89d33631743d2544b5c7b46
AbHXn <asofficial47@gmail.com>
2025-10-26 15:21:33 +0530

    Update KNN.java

--- JavaKnn/KNN.java
+++ JavaKnn/KNN.java
@@ -3,6 +3,7 @@ package JavaKnn;
 import java.awt.image.BufferedImage;
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.HashMap;
 
 class KNN{
     // trainDir -> folder path for training images
@@ -10,14 +11,14 @@ class KNN{
     // runDir -> folder path of images to be classified
     // nCount -> neighbor count...
     public String trainDir, targetDir, runDir;
-	public int nCount;
+	public double dRate;
 	KNN(   String trainDir, 
            String targetDir, 
            String runDir, 
-           int nCount)
+           double dRate)
     {	
         this.trainDir = trainDir;
-		this.nCount = nCount;
+		this.dRate = dRate;
         this.targetDir = targetDir;
         this.runDir = runDir;
 	}
@@ -25,7 +26,7 @@ class KNN{
     // EUCLIDEAN DISTANCE
     private double get_euclidean_distance(double[] testImage, double[] trainImage){
         double distance = 0;
-        for(int i = 0; i < TrainData.PIXEL_SIZE; i++){
+        for(int i = 0; i < testImage.length; i++){
             double diff = testImage[i] - trainImage[i];
             distance += diff * diff;
         }
@@ -41,28 +42,66 @@ class KNN{
             // concurrent running
             ExecutorService executor = Executors.newFixedThreadPool(5);
             for(Image trainImage: TrainData.trainImages){
+				final Image tImage = trainImage;
                 executor.submit(() -> {
-                    double distance = get_euclidean_distance(testVec, trainImage.features);
+                    double distance = get_euclidean_distance(testVec, tImage.features);
                     // set the distance attribute
-                    trainImage.set_distance(distance);
+                    tImage.set_distance(distance);
                 });
             }
             executor.shutdown();
-            executor.awaitTermination(1, TimeUnit.MINUTES);
+            if(!executor.awaitTermination(10, TimeUnit.MINUTES)){
+				System.out.println("Some thread not finished work");
+			};
             // sort images based on distance
             TrainData.trainImages.sort((imga, imgb) -> {
                 return Double.compare(imga.distance, imgb.distance); 
             });
+            // System.out.println("Printing the distance in sorted order: ");
+            // for(int x = 0; x < TrainData.trainImages.size(); x++){
+            //     Image current_image = TrainData.trainImages.get(x);
+            //     System.out.println("Image: " + current_image.name);
+            //     System.out.println("distance: " + current_image.distance);
+            // }
             // get the neighbours
-            ArrayList<String> neighbours = new ArrayList<>();
+            //ArrayList<String> neighbours = new ArrayList<>();
             Classifier classifier = new Classifier(this.targetDir);
-            for(int x = 0; x < nCount; x++)
-                neighbours.add(classifier.getFileNameOnly(
-                    TrainData.trainImages.get(x).name));
+
+            int nCount = (int) (TrainData.trainImages.size() * dRate);
+            nCount = Math.max(1, Math.min(nCount, TrainData.trainImages.size())); // clamp
+
+            HashMap<String, Double> classWeights = new HashMap<>();
+            double totalWeight = 0.0;
+
+            for(int x = 0; x < nCount; x++){
+                Image neighbor = TrainData.trainImages.get(x);
+                String className = classifier.getFileNameOnly(neighbor.name);
+
+                double weight = 1.0 / (neighbor.distance + 1e-6);
+                classWeights.put(className, classWeights.getOrDefault(className, 0.0) + weight);
+                totalWeight += weight;
+            }
+
+            // Find class with highest total weight
+            String predictedClass = "";
+            double maxWeight = -1.0;
+
+            for(String cls : classWeights.keySet()){
+                double w = classWeights.get(cls);
+                double percentage = (w / totalWeight) * 100.0;
+                System.out.println(cls + ": " + percentage + "%");
+                if(w > maxWeight){
+                    maxWeight = w;
+                    predictedClass = cls;
+                }
+            }
+
+            System.out.println("Predicted Class: " + predictedClass);
+
             // extract the folder name from neighbours images name
-            String folder_name = classifier.folder_name(neighbours);
+            //String folder_name = classifier.folder_name(neighbours);
             // move to the folder where neighbours live...
-            classifier.move(sourceImage, folder_name, imageName);
+            classifier.move(sourceImage, predictedClass, imageName);
             return true;
         }catch(Exception e){
             System.out.println(e);
